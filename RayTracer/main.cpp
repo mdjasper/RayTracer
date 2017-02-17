4aaf8f332d05457ad496dbe9e706936811aa820a
Michael Jasper <mdjasper@ldschurch.org>
2017-02-16 21:13:07 -0700

    Initial Commit

---
+++ RayTracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RayTracer
+//
+//  Created by Michael Jasper on 2/16/17.
+//  Copyright © 2017 Michael Jasper. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
