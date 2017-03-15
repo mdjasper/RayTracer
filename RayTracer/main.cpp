8572fcc07b0588e7b19f5932f97bd90becb7963b
Michael Jasper <mdjasper@ldschurch.org>
2017-03-15 17:05:17 -0600

    Hidden Mickey Shadow

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -40,16 +40,13 @@ int main()
     Sphere *ball = new Sphere({0,0,10}, 2, blue);
     ShapeList::getInstance().addShape(std::unique_ptr<Shape>(ball));
     
-    Sphere *ball2 = new Sphere({2,2,12}, 0.5, {255,255,255});
+    Sphere *ball2 = new Sphere({1,4,9}, 1, green);
     ShapeList::getInstance().addShape(std::unique_ptr<Shape>(ball2));
     
+    Sphere *ball3 = new Sphere({0,5,12}, 1, green);
+    ShapeList::getInstance().addShape(std::unique_ptr<Shape>(ball3));
     
-//    Plane *ground = new Plane(Point{0,0,0}, Vector{0,1,0}, red);
-//    ShapeList::getInstance().addShape(std::unique_ptr<Shape>(ground));
-    
-//    Triangle *tri = new Triangle({-1,0,5}, {1,0,5}, {0,1,3}, red);
-Triangle *tri = new Triangle({-1000,-4,1000}, {1000,-4,1000}, {0,-4,1}, red);
-//    Triangle *tri = new Triangle({-50,-50,10}, {50,-50,10}, {0,50,10}, red);
+    Triangle *tri = new Triangle({-1000,-2,1000}, {1000,-2,1000}, {0,-2,1}, red);
     ShapeList::getInstance().addShape(std::unique_ptr<Shape>(tri));
     
     
